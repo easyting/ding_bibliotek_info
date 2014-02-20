b8ecd0cc9b126db1ec9d1ee37c9d303fd8397e1a
AnatolyMuntean <am@inlead.dk>
2014-02-20 16:47:20 +0200

    case 3816 - Remove the pane when no results.

---
+++ js/ding_bibliotek_info.scripts.js
@@ -0,0 +1,9 @@
+(function ($) {
+  Drupal.behaviors.ding_bibliotek_info = {
+    attach: function (context, settings) {
+      if ($('.pane-search-result ol.search-results').length === 0) {
+        $('.pane-bibliotek-info').remove();
+      }
+    }
+  };
+}(jQuery));
